2c4a976c388a1c5e6c62f6fe49cdbeecc2e0e9e5
VNATAS3 <sathishkumar.natarajan1@pearson.com>
2022-08-29 20:01:22 +0530

    here i explain about arithmetic operators in js

---
+++ operators.js
@@ -0,0 +1,36 @@
+//1)arithmetic operator(+,-,*,/,%,**,++,--)
+// ++a -->pre increment , a++ -->post increment
+// --a -->pre decremen , a-- -->post decrement
+// % -->it return reminder.
+// / -->it return quotent.
+//note for increment and decrement (only one value performent will occcur that mean it will increase are decrease only one number)
+//pre opertator
+//--------------------------------- (++a -->pre increment)----------------------------
+// var a=10;
+// var b=++a //(a=a+1=11 here operatio will perform firsr now a=11 and that value of a is assigned to b a,b are now 11)
+// alert("value of a is" + a);
+// alert("value of b is" + b);
+// ------------------------------- (--a -->pre decrement)---------------------------
+// var c=10;
+// var d=--c //(c=c-1=9 here operatio will perform firsr now value is c=9 and that value of c is assigned td b c,d are now 9)
+// alert("value of c is" + c);
+// alert("value of d is" + d);
+//--------------------------------- (a++ -->post increment)----------------------------
+// var a=10;
+// var b=a++ //(here the value of a is first assign to b now b value is 10, after then the operation perform a=a+1=11 now the value of a is ll)
+// alert("value of a is" + a);
+// alert("value of b is" + b);
+// ------------------------------- (a-- -->pre decrement)---------------------------
+// var c=10;
+// var d=c-- //(here the value of c is first assign to b now d value is 10, after then the operation perform c=c-1=9 now the value of c is 9)
+// alert("value of c is" + c);
+// alert("value of d is" + d);
+//--------------------------------------(assigment operators)-----------------------------------
+//1) equal to(=) assign some value to variable we use this eg var a=30;
+//compound assigment operator (it consist both arithmetic operator along with assigment operator) compound assignement operator is short property (for performing operation and assignement)
+//eg a+=5 (this is maily used for loop  puropse) //eactly i did not get tought in which sceniorio we use in loops
+var a=5 ;
+ a += 5 ; //compound assigment operator act as a loop variable
+//c=a+=5 
+
+
